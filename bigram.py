5471b6209b83a87f3ea40f92a68a03d69fd11623
keanuspies <keanuspies@gmail.com>
2023-11-11 14:03:34 -0800

    adding new finetuned model

--- bigram.py
+++ bigram.py
@@ -52,12 +52,13 @@ n = int(0.9*len(data))
 train_data = data[:n]
 val_data = data[n:]
 
+# data loading
 def get_batch(split):
+    # generate a small batch of inputs and targets
     data = train_data if split == 'train' else val_data
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i:i+block_size] for i in ix])
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-
     x, y = x.to(device), y.to(device)
     return x, y
 
